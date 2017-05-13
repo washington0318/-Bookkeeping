049323aeb7866b028adee9ecd6724e112db1700f
Your Name <you@example.com>
2017-05-13 22:49:47 +0800

    homework2 remark>5

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using WebApplication1.Models;
 
@@ -7,41 +8,63 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            Random ran = new Random();
-            var model = new List<Myclass>();
-            Myclass note;
-            for (int i = 0; i <= 200; i++)
-            {
-                note = new Myclass();
-                int randnumber = ran.Next(2);//0、1、2
-                if (randnumber != 1)
-                {
-                    note.Type = "支出";
-                }
-                else
-                {
-                    note.Type = "收入";
-                }
-                string year = DateTime.Now.Year.ToString();
-                var month = ran.Next(12);
-               
-                //亂數範圍(日期)
-                Random day = new Random();
-                int MinValue = 1;
-                int MaxValue = 30;
-                var day1 = ran.Next(MinValue, MaxValue).ToString();
-               //目前缺少時間變化
+        //public ActionResult Index()
+        //{
+        //    Random ran = new Random();
+        //    var model = new List<Myclass>();
+        //    Myclass note;
+        //    for (int i = 0; i <= 200; i++)
+        //    {
+        //        note = new Myclass();
+        //        //int randnumber = ran.Next(2);//0、1、2
+        //        //if (randnumber != 1)
+        //        //{
+        //        //    note.Type = "支出";
+        //        //}
+        //        //else
+        //        //{
+        //        //    note.Type = "收入";
+        //        //}
+        //       // string year = DateTime.Now.Year.ToString();               
+        //       // var month = ran.Next(12);
 
-                note.Date = year +"/"+ month + "/"+day1;
-                //note.Date = DateTime.Now.AddDays((ran.Next(3650)));            
-                note.Money = ran.Next(1000);
-                model.Add(note);
-            }
-            return View(model);
-        }
+        //       // //亂數範圍(日期)
+        //       // Random day = new Random();
+        //       // int MinValue = 1;
+        //       // int MaxValue = 30;
+        //       // var day1 = ran.Next(MinValue, MaxValue).ToString();
+        //       ////目前缺少時間變化
 
+        //       // //note.Date = year +"/"+ month + "/"+day1;
+        //       // //note.Date = DateTime.Now.AddDays((ran.Next(3650)));            
+        //       // note.Money = ran.Next(1000);
+        //        //model.Add(note);
+        //    }
+        //    return View(model);
+        //}
+
+            
+            public ActionResult Index()
+                {
+                    Model2 db = new Model2();
+                    var model = new List<Myclass>();
+                    Myclass note;
+                    
+                foreach (var i in db.AccountBook.ToList()) 
+                        {
+                            note = new Myclass();
+                            //note.Type = i.Categoryyy.ToString();
+                            note.Date = i.Dateee.ToString("yyyy-MM-dd");
+                            note.Money = i.Amounttt.ToString("###,###");
+                note.Other = (i.Remarkkk.Length > 5) ? i.Remarkkk.Substring(0, 5) : "地雷";
+                            var 類型 = "";
+                            if (i.Categoryyy == 0) { 類型 = "支出"; }
+                            else { 類型 = "收入"; }
+                            note.Type = 類型.ToString();
+                            model.Add(note);
+                        }
+                    return View(model);
+                }
             public ActionResult About()
             {
                 ViewBag.Message = "Your application description page.";
@@ -55,5 +78,6 @@ namespace WebApplication1.Controllers
 
                 return View();
             }
-        }
+       
+    }
  }
--- WebApplication1/Models/Myclass.cs
+++ WebApplication1/Models/Myclass.cs
@@ -8,9 +8,10 @@ namespace WebApplication1.Models
     public class Myclass
     {
         public string Type { get; set; }
-        public int Money { get; set; }
-        //public DateTime Date { get; set; }
+        public string Money { get; set; }//money 型別要用 decimal
         public string Date { get; set; }
+        public string Other { get; set; }
+        //public string Date { get; set; }
 
     }
 }
